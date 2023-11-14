b3c11b5a06273884bcfecb86389ee7c55699f9ea
ibrkorkut07 <ibr.kor@gmail.com>
2023-11-14 23:53:54 +0100

    git14 matchers

---
+++ src/test/java/tests/withPojoClass/a_Matchers/solved/Get13.java
@@ -0,0 +1,49 @@
+package tests.withPojoClass.a_Matchers.solved;
+
+import baseUrl.TestBaseUrls;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import pojos.Bookingdates;
+import pojos.SingleBooking;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Get13 extends TestBaseUrls {
+    /*
+	    When I send a GET request to REST API URL
+		     https://restful-booker.herokuapp.com/booking/5
+	    Then HTTP Status Code should be 200
+		And response content type is “application/JSON”
+		And response body should be as follows;
+		    {
+		     "firstname": "Sally",
+		     "lastname": "Ericsson",
+		     "totalprice": 111,
+		     "depositpaid": false,
+		     "bookingdates": {
+		                      "checkin": "2017-05-23",
+		                      "checkout":"2019-07-02" }
+	                          }
+	         "additionalneeds": "Breakfast"
+	*/
+
+    @Test
+    public void get13MatchersWithPojo () {
+        restfulSpec.pathParam("first", "5");
+        Response response = given().spec(restfulSpec).when().get("{first}");
+        Bookingdates expBookingDates = new Bookingdates("2017-05-23", "2019-07-02");
+        SingleBooking expData = new SingleBooking("Sally", "Ericsson", 111, false, expBookingDates, "Breakfast");
+
+        response.then().statusCode(200).contentType(ContentType.JSON).
+                body("firstname", equalTo(expData.getFirstname()),
+                        "lastname", equalTo(expData.getLastname()),
+                                "totalprice", equalTo(expData.getTotalprice()),
+                                "depositpaid", equalTo(expData.isDepositpaid()),
+                                "bookingdates.checkin", equalTo(expData.getBookingdates().getCheckin()),
+                                "bookingdates.checkout", equalTo(expData.getBookingdates().getCheckout()) );
+    }
+
+}
---
+++ src/test/java/tests/withPojoClass/a_Matchers/solved/Get14.java
@@ -0,0 +1,43 @@
+package tests.withPojoClass.a_Matchers.solved;
+
+import baseUrl.TestBaseUrls;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+import pojos.Bookingdates;
+import pojos.SingleBooking;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Get14 extends TestBaseUrls {
+
+    /*
+        When I send GET Request to
+             https://restful-booker.herokuapp.com/booking/3
+		Then Status code is 200
+		And Content type is "application/json"
+		And Status line is "HTTP/1.1 200 OK"
+		And First name is Jim
+		And Total price is 623
+		And Deposit paid is true
+		And Checkin date is "2020-03-18"
+
+		Use De-Serialization to convert Json response body to a Map.
+		Then by using the map and soft-assertion make assertions.
+    */
+
+    @Test
+    public void get14MatchersWithPojo () {
+        restfulSpec.pathParam("first", "3");
+        Response response = given().spec(restfulSpec).when().get("{first}");
+        Bookingdates expBookingDates = new Bookingdates("2020-03-18", "");
+
+        response.then().statusCode(200).contentType(ContentType.JSON).statusLine("HTTP/1.1 200 OK").
+                body("firstname", equalTo("Jim"),
+                        "totalprice", equalTo(623),
+                        "depositpaid", equalTo(true),
+                        "bookingdates.checkin", equalTo(expBookingDates.getCheckin()) );
+    }
+
+}
